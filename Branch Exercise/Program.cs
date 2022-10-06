d364619e9be5d0711ee74b04de7900a2f8d67433
Sean <ripper.sean@yahoo.com>
2022-10-06 09:55:14 -0400

    Main Branch

---
+++ Branch Exercise/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Branch_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Branch");
+        }
+    }
+}
